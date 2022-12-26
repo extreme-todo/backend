6bab2ec6eaa631c3cb4735f878762970f228e8e2
FerrariRoma <win9592@gmail.com>
2022-12-26 18:30:13 +0900

    Chore : 주석 삭제

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todo } from './entities/todo.entity';
 import TodoController from './todo.controller';
 import { TodoService } from './todo.service';
 
 @Module({
-  imports: [
-    // ConfigModule,
-    TypeOrmModule.forFeature([Todo]),
-  ],
+  imports: [TypeOrmModule.forFeature([Todo])],
   controllers: [TodoController],
   providers: [TodoService],
 })
