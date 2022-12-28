172e259d628f69006bcac34130165cccf9619e71
FerrariRoma <win9592@gmail.com>
2022-12-28 22:33:24 +0900

    Update : User Entity 수정 및 모듈에 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  //TODO: User 엔티티 추가
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
   controllers: [UserController],
 })
