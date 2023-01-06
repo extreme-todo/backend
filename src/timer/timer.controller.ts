290e2daa9b6f331cea1b8228feb07a621b08115d
imhjnoh <imhjnoh@gmail.com>
2023-01-06 19:18:55 +0900

    Update: Timer controller #3

--- src/timer/timer.controller.ts
+++ src/timer/timer.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TimerService } from './timer.service';
+import { User } from 'src/user/entities/user.entity';
 
 @Controller('timer')
-export class TimerController {}
+export class TimerController {
+  constructor(private timerService: TimerService) {}
+
+  // TODO: AuthGuard 등으로 가져온 유저를 사용할 예정
+  @Get('total_focus')
+  getTotalFocusTime() {
+    const fakeUser = { email: 'asd@asd.asd', username: 'asd' } as User;
+    return this.timerService.getTotalFocusTime(fakeUser);
+  }
+
+  // TODO: AuthGuard 등으로 가져온 유저를 사용할 예정
+  @Get('total_rest')
+  getTotalRestTime() {
+    const fakeUser = { email: 'asd@asd.asd', username: 'asd' } as User;
+    return this.timerService.getTotalRestTime(fakeUser);
+  }
+
+  @Get('progress')
+  getProgress() {
+    // TODO: 추이 로직 작성
+  }
+}
