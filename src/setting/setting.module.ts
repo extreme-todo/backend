44156b49cf91f78353abc2ef67a90cc333a0ce97
imhjnoh <imhjnoh@gmail.com>
2023-01-22 16:56:54 +0900

    Update: setting 모듈 typeorm 모듈 추가 #5

--- src/setting/setting.module.ts
+++ src/setting/setting.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Setting } from './entities/setting.entity';
 import { SettingController } from './setting.controller';
 import { SettingService } from './setting.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Setting])],
   controllers: [SettingController],
-  providers: [SettingService]
+  providers: [SettingService],
 })
 export class SettingModule {}
