73ea8ba32957ebaa1b26e2b0a22c75b1e2e61ce5
imhjnoh <imhjnoh@gmail.com>
2023-01-22 17:21:54 +0900

    Update: setting 컨트롤러 구현 완료 #5

--- src/setting/setting.controller.ts
+++ src/setting/setting.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+import { UpdateSettingDto } from './dto/update-setting.dto';
+import { SettingService } from './setting.service';
 
 @Controller('setting')
-export class SettingController {}
+export class SettingController {
+  constructor(private settingService: SettingService) {}
+  // TODO: AuthGuard 등으로 가져온 유저를 사용할 예정
+  @Get('/')
+  getSetting() {
+    const fakeUser = { email: 'asd@asd.asd', username: 'asd', id: 1 } as User;
+    return this.settingService.find(fakeUser);
+  }
+
+  // TODO: AuthGuard 등으로 가져온 유저를 사용할 예정
+  @Patch('/')
+  udpateSetting(@Body() data: UpdateSettingDto) {
+    const fakeUser = { email: 'asd@asd.asd', username: 'asd', id: 1 } as User;
+    return this.settingService.update(fakeUser, data);
+  }
+}
