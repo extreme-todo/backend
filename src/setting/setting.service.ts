35d0b0b5067bca19bb8165c4ba6f96acbb1d16af
imhjnoh <imhjnoh@gmail.com>
2023-01-22 17:17:50 +0900

    Update: setting 서비스 구현 완료 #5

--- src/setting/setting.service.ts
+++ src/setting/setting.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { UpdateSettingDto } from './dto/update-setting.dto';
+import { Setting } from './entities/setting.entity';
 
 @Injectable()
-export class SettingService {}
+export class SettingService {
+  constructor(@InjectRepository(Setting) private repo: Repository<Setting>) {}
+
+  async init(user: User): Promise<Setting> {
+    const setting = this.repo.create();
+    setting.user = user;
+    return await this.repo.save(setting);
+  }
+
+  async reset(user: User): Promise<Setting> {
+    const setting = await this.repo.findOne({ where: { user } });
+    setting.colorMode = 'auto';
+    setting.extremeMode = true;
+    return await this.repo.save(setting);
+  }
+
+  async update(user: User, data: UpdateSettingDto): Promise<Setting> {
+    const setting = await this.repo.findOne({ where: { user } });
+    setting.colorMode = data.colorMode;
+    setting.extremeMode = data.extremeMode;
+    return await this.repo.save(setting);
+  }
+}
