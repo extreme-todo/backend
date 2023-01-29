e9b1e92b467b8ebafa5c8a073b02220c5b009282
FerrariRoma <win9592@gmail.com>
2023-01-29 21:47:36 +0900

    Update : user 검색 메소드 추가하고 serialize interceptor 적용 #2

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,40 @@
-import { Controller, Get, Query, Redirect } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Query,
+  Redirect,
+} from '@nestjs/common';
+import { Serialize } from 'src/interceptor/serialize.interceptor';
+import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
-@Controller('api/users')
+@Controller('/api/users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get('callback/google/start')
+  @Get('/callback/google/start')
   @Redirect('', 302)
   googleSignUp() {
     const url = this.userService.googleLoginApi();
     return { url };
   }
 
-  @Get('callback/google/finish')
+  @Get('/callback/google/finish')
   googleCallback(@Query() authCode: string) {
     return this.userService.googleCallback(authCode);
+  }
+
+  @Serialize(UserDto)
+  @Get('/:email')
+  async findUser(@Param('email') email: string) {
+    const user = await this.userService.findOne(email);
+    console.log(user);
+    if (!user) {
+      console.log('여기 옴');
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
 }
