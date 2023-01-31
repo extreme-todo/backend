d767ff3c09748f33bd22bcbf815e440b3c7636ad
imhjnoh <imhjnoh@gmail.com>
2023-01-31 23:35:35 +0900

    Update : Category entity의 이름을 unique로 수정 #6

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -13,7 +13,7 @@ export class Category {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
   name: string;
 
   @ManyToMany(() => Todo, (todo) => todo.categories)
