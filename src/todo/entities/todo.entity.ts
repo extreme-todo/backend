032bbe84d444d46c8543ca41afeb047a12b5a730
imhjnoh <imhjnoh@gmail.com>
2023-02-03 04:28:18 +0900

    Update : Todo entity 업데이트 #4

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,12 +1,12 @@
 import { User } from 'src/user/entities/user.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Todo {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ type: 'timestamp' })
   date: Date;
 
   @Column()
@@ -19,9 +19,10 @@ export class Todo {
   done: boolean;
 
   @ManyToOne(() => User, (user) => user.todo)
+  @JoinColumn()
   user: User;
 
   // TODO : 수능 영어 단어 => 이처럼 여러 가지 카테고리가 중복될 수 있지 않을까?
-  @Column()
+  @Column({ default: null })
   category: string;
 }
