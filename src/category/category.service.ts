1b9106e1b642a53e064faf90dc407d2568e5fa5f
imhjnoh <imhjnoh@gmail.com>
2023-02-04 01:02:01 +0900

    Fix : Category service myCategories 수정 #6
    find option 수정

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -6,35 +6,43 @@ import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
-    constructor(@InjectRepository(Category) private repo: Repository<Category>){}
+  constructor(@InjectRepository(Category) private repo: Repository<Category>) {}
 
-    async myCategories (user: User) {
-        const categories = await this.repo.find({where: {author: user}});
-        return categories
+  async myCategories(user: User) {
+    console.log(user);
+    const categories = await this.repo.find({
+      relations: { author: true },
+      where: { author: { id: user.id } },
+    });
+    return categories;
+  }
+
+  async findOrCreateCategories(
+    user: User,
+    categories: string[],
+  ): Promise<Category[]> {
+    if (!categories) return null;
+    const newCategories = await Promise.all(
+      categories.map(async (category) => {
+        const searched = await this.find(category);
+        if (searched) return searched;
+        else return await this.create(user, category);
+      }),
+    );
+    return newCategories;
+  }
+
+  async find(name: string): Promise<Category> | null {
+    const category = await this.repo.findOne({ where: { name } });
+    return category;
+  }
+
+  async create(user: User, name: string) {
+    try {
+      const category = this.repo.create({ name, author: user });
+      return await this.repo.save(category);
+    } catch (err) {
+      throw new BadRequestException('Category already exists');
     }
-
-    async findOrCreateCategories (user: User, categories: string[]): Promise<Category[]> {
-        if(!categories) return null;
-        const newCategories = await Promise.all(categories.map(async category => {
-            const searched = await this.find(category);
-            if(searched) return searched;
-            else return await this.create(user, category);
-        }))
-        return newCategories;
-    }
-
-    async find (name: string): Promise<Category> | null {
-        const category = await this.repo.findOne({where: {name}})
-        return category
-    }
-
-    async create(user: User, name: string){
-        try{
-            const category = this.repo.create({name, author: user})
-            return await this.repo.save(category)
-        }catch(err){
-            throw new BadRequestException('Category already exists')
-        }
-    }
-
+  }
 }
