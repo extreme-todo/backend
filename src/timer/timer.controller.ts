f8dc37ac0f84ad213bf9cd70a909f77ae916e5be
imhjnoh <imhjnoh@gmail.com>
2023-02-06 18:30:53 +0900

    Update : 집중시간, 휴식시간 업데이트 route 추가 #3

--- src/timer/timer.controller.ts
+++ src/timer/timer.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { TimerService } from './timer.service';
 import { User } from 'src/user/entities/user.entity';
 import { CurrentUser } from 'src/user/decorators/current-user.decorator';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { UpdateFocusDto } from './dto/update-focus.dto';
+import { UpdateRestDto } from './dto/update-rest-dto';
 
 @Controller('api/timer')
 export class TimerController {
@@ -24,5 +26,23 @@ export class TimerController {
   @UseGuards(AuthGuard)
   getProgress(@CurrentUser() user: User) {
     return this.timerService.getProgress(user);
+  }
+
+  @Patch('total_focus')
+  @UseGuards(AuthGuard)
+  updateTotalFocusTime(
+    @Body() focusData: UpdateFocusDto,
+    @CurrentUser() user: User,
+  ) {
+    return this.timerService.updateFocusTime(focusData.addFocusTime, user);
+  }
+
+  @Patch('total_rest')
+  @UseGuards(AuthGuard)
+  updateTotalRestTime(
+    @Body() restData: UpdateRestDto,
+    @CurrentUser() user: User,
+  ) {
+    return this.timerService.updateRestTime(restData.addRestTime, user);
   }
 }
