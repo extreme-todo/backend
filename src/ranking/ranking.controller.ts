cb36563c3497f08462562cc60108273ef95f3170
FerrariRoma <win9592@gmail.com>
2023-02-14 22:08:05 +0900

    Create : #13 ranking controller 생성
    add todo dto 주석 수정

--- src/ranking/ranking.controller.ts
+++ src/ranking/ranking.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { User } from 'src/user/entities/user.entity';
+import { RankingService } from './ranking.service';
+import { CurrentUser } from 'src/user/decorators/current-user.decorator';
 
-@Controller('ranking')
-export class RankingController {}
+@Controller('/api/ranking')
+@UseGuards(AuthGuard)
+export class RankingController {
+  constructor(private rankingService: RankingService) {}
+
+  @Get('')
+  ranking(@Query('category') category: string, @CurrentUser() user: User) {
+    return this.rankingService.ranking(category, user);
+  }
+}
